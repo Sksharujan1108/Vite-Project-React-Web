0b07cd086f3aa22c29431146b825e509ad4eeed3
@sarujan1 <sarujan@codelantic.com>
2024-04-07 19:46:49 +0530

    update

--- EP-05-UseEffect/Hooks-2-UseEffect/src/component/profile/index.jsx
+++ EP-05-UseEffect/Hooks-2-UseEffect/src/component/profile/index.jsx
@@ -1,34 +1,31 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 
 const ProfilePage = () => {
-    const [num1, setnum1] = useState(100);
-    const [num2, setnum2] = useState(2000);
+    const [num1, setNum1] = useState(100);
+    const [num2, setNum2] = useState(2000);
 
     useEffect(() => {
-        setnum1(150);
+        console.log('Inside useEffect');
+        setNum1(150);
 
         return () => {
-            setnum1(100)
-            console.log( "Mwmorycleanup", setnum1 );
+            console.log('Cleanup in useEffect');
+            setNum1(''); // Reverting num1 back to its default value
         };
     }, [num1, num2]);
 
-    console.log('num1', num1 ); // 150
+    console.log('num1', num1); // 150
 
-  return ( 
-    <div>
-      <h1> ProfilePage </h1>
-        <p> num1: {num1} </p>
-          <button 
-            onClick = {() => setnum1(num1 + 1)}
-          > Click me </button>
+    return ( 
+        <div>
+            <h1> ProfilePage </h1>
+            <p> num1: {num1} </p>
+            <button onClick={() => setNum1(num1 + 1)}> Click me </button>
 
-        <p> num2: {num2} </p>
-          <button 
-            onClick={() => setnum2(num2 + 1)}
-          > Add me </button>
-    </div>
-  )
-}
+            <p> num2: {num2} </p>
+            <button onClick={() => setNum2(num2 + 1)}> Add me </button>
+        </div>
+    );
+};
 
 export default ProfilePage;
